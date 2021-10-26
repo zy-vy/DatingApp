814f7471b16f83ec7929b3a0afaf35f10e4cb2e4
zy-vy <jifidoank@gmail.com>
2021-10-26 09:10:31 +0700

    end of section 2

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public UsersController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await this.context.Users.ToListAsync();
+        }
+        // api/users/3 
+        [HttpGet("{id}")]
+        public async  Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await this.context.Users.FindAsync(id);
+            
+        }
+
+    }
+}
