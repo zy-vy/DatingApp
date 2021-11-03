bc14f29b0414ec5871ffaec8d2b56b491c2dc8ff
zy-vy <jifidoank@gmail.com>
2021-11-03 10:03:11 +0700

    End of section 3

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -34,6 +34,7 @@ namespace DatingApp
                 options.UseSqlite(this.config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DatingApp", Version = "v1" });
@@ -53,6 +54,8 @@ namespace DatingApp
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors( x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
